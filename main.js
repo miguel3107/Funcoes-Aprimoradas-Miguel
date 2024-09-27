f302a719d25313bc48686dbccb937b043b57f955
miguel3107 <108474194+miguel3107@users.noreply.github.com>
2024-09-27 10:11:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("clik",() => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+
+function respostaSelecionada(opcaoSelecionada){
+     const afirmacoes = opcaoSelecionada.afirmacao;
+     historiaFinal += afirmacoes + " ";
+     atual++;
+     mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
